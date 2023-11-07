16f3258682a46f9052c2fcf722491a5d27d4e5a2
Jack Cassidy <jcassidy@iastate.edu>
2023-11-07 12:04:23 -0600

    finished validation

--- public/main.spec.js
+++ public/main.spec.js
@@ -2,6 +2,18 @@
 import userModel from './main.js';
 import {test, expect} from 'vitest'
 
-test('validateName returns true', () => {
-    expect(userModel.validateName('John')).toBe(true);
+//postivite tests 
+test('user Input returns true', () => {
+    expect(userModel.validateName('Jack')).toBe(true);
+    expect(userModel.validateName('Cassidy')).toBe(true);
+    expect(userModel.validateEmail('jcassidy@iastate.edu')).toBe(true);
+    expect(userModel.validateAddress("123 Fake Street")).toBe(true);
+    expect(userModel.validateAddress("Apartment 73")).toBe(true);
+    expect(userModel.validateAddress("Ames")).toBe(true);
+    expect(userModel.validateAddress("Iowa")).toBe(true);
+    expect(userModel.validateZipCode("50014")).toBe(true);
+  });
+  //negative tests 
+  test('user Input returns false', ()=> {
+
   });
